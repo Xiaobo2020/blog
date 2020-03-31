386f8ecd29c50dd3cb9e037aba6f7d54213b5306
Xiaobo2020 <1971822513@qq.com>
2020-03-31 23:09:06 +0800

    [update] draft - DIY - promise.js

--- drafts/promise.js
+++ drafts/promise.js
@@ -1,24 +1,20 @@
 function CPromise (executor) {
   this.status = 'pending';
   this.data;
-  this.onfulfilledListeners = [];
-  this.onrejectedListeners = [];
+  this.listeners = [];
 
-  const emitter = (handlers) => (data) => {
-    for (let i = 0; i < handlers.length; i++) {
-      handlers[i](data);
-    }
-  };
-  const toggle = (status) => (handler) => (data) => {
+  const transition = (status) => (data) => {
     if (this.status === 'pending') {
       // 一次性状态翻转
       this.status = status;
       this.data = data;
-      handler(data);
+      for (let i = 0; i < this.listeners.length; i++) {
+        this.listeners[i]();
+      }
     }
   };
-  const resolve = toggle('fulfilled')(emitter(this.onfulfilledListeners));
-  const reject = toggle('rejected')(emitter(this.onrejectedListeners));
+  const resolve = transition('fulfilled');
+  const reject = transition('rejected');
 
   try {
     executor(resolve, reject);
@@ -27,76 +23,58 @@ function CPromise (executor) {
   }
 }
 
-// 暂时只处理 fulfilled 和 rejected
-const nextPromiseGenerator = (data) => (status) => (handler) => {
-  if (typeof handler === 'function') {
-    let v;
-    try {
-      v= handler(data);
-      if (v instanceof CPromise || v instanceof Promise) {
-        return v;
-      } else {
-        return new CPromise((resolve, reject) => resolve(v));
-      }
-    } catch (e) {
-      return new CPromise((resolve, reject) => reject(e));
+const procedureGenerator = (resolve, reject) => (promise2, x) => {
+  const promiseResolutionProcedure = procedureGenerator(resolve, reject);
+  if (promise2 === x) {
+    throw new TypeError(x);
+  } else if (x instanceof CPromise) {
+    x.then(resolve, reject);
+  } else if (typeof x === 'function' || (typeof x === 'object' && x !== null)) {
+    const then = x.then;
+    if (typeof then === 'function') {
+      then.call(
+        x,
+        y => {
+          promiseResolutionProcedure(promise2, y);
+        },
+        r => {
+          reject(r);
+        },
+      )
+    } else {
+      resolve(x);
     }
   } else {
-    return new CPromise((resolve, reject) => {
-      if (status === 'fulfilled') {
-        resolve(data);
-      } else {
-        reject(data);
-      }
-    });
-  }
-}
-
-CPromise.prototype.then = function (onfulfilled, onrejected) {
-  const getNextPromise = nextPromiseGenerator(this.data)(this.status);
-  if (this.status === 'fulfilled') {
-    return getNextPromise(onfulfilled);
-  } else if (this.status === 'rejected') {
-    return getNextPromise(onrejected);
-  } else {
-    return new CPromise((resolve, reject) => {
-      this.onfulfilledListeners.push((value) => {
-        nextPromiseGenerator(value)('fulfilled')(onfulfilled).then(resolve, reject);
-      });
-      this.onrejectedListeners.push((reason) => {
-        nextPromiseGenerator(reason)('rejected')(onrejected).then(resolve, reject);
-      });
-    });
+    resolve(x)
   }
 };
 
-const promise = new CPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1);
-  }, 2000);
-  // resolve(1);
-});
+CPromise.prototype.then = function (onResolve, onReject) {
+  const onFulfilled = typeof onResolve === 'function' ? onResolve : v => v;
+  const onRejected = typeof onReject === 'function' ? onReject : r => {throw r};
 
-promise
-  .then(
-    (res) => {
-      console.log('onfulfilled1', res);
-      return new Promise((resolve, reject) => reject('Hello'));
-    },
-    (err) => console.log('onrejected1', err)
-  )
-  .then(
-    res => console.log('onfulfilled2', res),
-    err => {
-      console.log('onrejected2', err);
-      return new CPromise((resolve, reject) => reject('World'));
+  const promise2 = new CPromise((resolve, reject) => {
+    const promiseResolutionProcedure = procedureGenerator(resolve, reject);
+    const fn = () => {
+      setTimeout(() => {
+        try {
+          const x = this.status === 'fulfilled' 
+            ? onFulfilled(this.data) 
+            : onRejected(this.data);
+          promiseResolutionProcedure(promise2, x);
+        } catch (e) {
+          reject(e);
+        }
+      }, 0);
+    };
+    if (this.status === 'pending') {
+      this.listeners.push(fn);
+    } else {
+      fn();
     }
-  )
-  .then(
-    res => console.log('onfulfilled3', res),
-    err => console.log('onrejected3', err),
-  );
+  });
 
-// onfulfilled1 1
-// onrejected2 Hello
-// onrejected2 World
+  return promise2;
+};
+
+module.exports = CPromise;
